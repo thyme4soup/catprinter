a9661836fbbbb901d0f659d29193e81ee6453dae
thyme4soup <mchenevey7@gmail.com>
2021-10-21 03:03:42 -0700

    Install requirements through setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name="catprinter",
     version="1.0",
     packages=find_packages(),
+    install_requires=required,
     project_urls={
         'Source': 'https://github.com/thyme4soup/catprinter',
         'Fork of': 'https://github.com/rbaron/catprinter'
