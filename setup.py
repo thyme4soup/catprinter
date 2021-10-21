e3019f931642a97c56706dae5a0470ec8aa826aa
Michael Chenevey <mchenevey7@gmail.com>
2021-10-21 02:15:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="1.0",
     packages=find_packages(),
     project_urls={
-        'Source': 'https://github.com/thyme4soup/catprinter'
+        'Source': 'https://github.com/thyme4soup/catprinter',
         'Fork of': 'https://github.com/rbaron/catprinter'
     }
 )
